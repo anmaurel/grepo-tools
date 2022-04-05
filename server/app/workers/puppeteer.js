c3b861ed6318a50cfc2a019c843a8bed8af2c5b0
Antoine Maurel <antoinemaurel1@gmail.com>
2022-04-05 11:49:44 +0200

    conf: puppeteer headless false

--- server/app/workers/puppeteer.js
+++ server/app/workers/puppeteer.js
@@ -5,7 +5,7 @@ export default {
         let browser = await puppeteer.launch({
             args: [`--window-size=1920,1080`, '--incognito', '--no-sandbox', '--disable-setuid-sandbox'],
             defaultViewport: null,
-            headless: false,
+            headless: true,
             ignoreHTTPSErrors: true,
         })
 
