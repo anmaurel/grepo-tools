094b84984ed0fcddcb91a71572d36b2a7dfe8cce
Antoine Maurel <antoinemaurel1@gmail.com>
2022-08-06 21:30:06 +0200

    update: two worlds farm

--- server/app.js
+++ server/app.js
@@ -1,22 +1,35 @@
 import 'dotenv/config'
+import readline from 'readline'
 
 import User from './app/classes/User'
 import { grepolis, puppeteer } from './app/workers/'
 import utils from './app/utils'
 
-(async () => {
-    let browser = await puppeteer.startBrowser()
+const worlds = [
+    {
+        usr: process.env.GREPO_USERNAME,
+        pswd: process.env.GREPO_PASSWORD,
+        name: process.env.GREPO_WORLD,
+    },
+    {
+        usr: process.env.GREPO2_USERNAME,
+        pswd: process.env.GREPO2_PASSWORD,
+        name: process.env.GREPO2_WORLD,
+    },
+]
 
-    let page = await browser.newPage()
-    page.setDefaultTimeout(10000)
-
+const main = async (world) => {
     try {
+        let browser = await puppeteer.startBrowser()
+        let page = await browser.newPage()
+        page.setDefaultTimeout(10000)
+
         await page.goto(`https://fr.grepolis.com/`, {
             waitUntil: 'networkidle2',
             timeout: 0,
         })
 
-        let user = new User(page, process.env.GREPO_USERNAME, process.env.GREPO_PASSWORD, process.env.GREPO_WORLD)
+        let user = new User(page, world.usr, world.pswd, world.name)
         await user.auth()
 
         for (let rep = 1; rep < 1000; rep++) {
@@ -29,4 +42,20 @@ import utils from './app/utils'
     } catch (error) {
         console.log(`error - ${error}`)
     }
-})()
+}
+
+const rl = readline.createInterface(process.stdin, process.stdout)
+rl.question('Which world ? ', (worldName) => {
+    switch (worldName) {
+        case 'milos':
+            main(worlds[0])
+            break
+        case 'ikaria':
+            main(worlds[1])
+            break
+        default:
+            console.log('world not found')
+            rl.close()
+            break
+    }
+})
