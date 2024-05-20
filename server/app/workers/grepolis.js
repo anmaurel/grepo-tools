23a52ac52cec7b8c1b1ad9266039245b920aa2b7
Antoine Maurel <antoinemaurel1@gmail.com>
2024-05-20 19:40:04 +0200

    :up: update

--- server/app/workers/grepolis.js
+++ server/app/workers/grepolis.js
@@ -2,14 +2,14 @@ import utils from '../utils'
 
 export default {
     async collectResources(page) {
+        await page.waitForTimeout(utils.random(1200, 10000))
         await page.waitForSelector('#quickbar_dropdown1', { timeout: 0 })
         await page.click('#quickbar_dropdown1 > div')
 
-        await page.waitForTimeout(utils.random(800, 1600))
+        await page.waitForTimeout(utils.random(800, 2000))
         await page.waitForSelector('.select_all', { timeout: 0 })
         await page.click('.select_all')
-        await page.waitForTimeout(utils.random(800, 5000))
+        await page.waitForTimeout(utils.random(800, 2000))
         await page.click('#fto_claim_button')
-        await page.waitForTimeout(utils.random(800, 1600))
     },
 }
